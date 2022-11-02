4265687947943ea0eb1ad7137495814c5ca47465
sharad28 <saradmishra28@gmail.com>
2022-11-02 14:03:12 +0530

    setup sucessful

--- init_setup.sh
+++ init_setup.sh
@@ -1,16 +1,13 @@
 echo [$(date)]: "START"
-export _VERSION_=3.9
+export _VERSION_=3.7.15
 echo [$(date)]: "creating environment with python ${_VERSION_}"
 conda create --prefix ./env python=${_VERSION_} -y
 echo [$(date)]: "activate environment"
 source activate ./env
-echo [$(date)]: "Initialize git"
-git init
-# echo [$date]: "Installation pycoco"
-# pip3 install "git+https://github.com/philferriere/cocoapi.git#egg=pycocotools&subdirectory=PythonAPI"
 echo [$(date)]: "install requirements"
 pip install -r requirements.txt
-
+echo [$(date)]: "Initialize git"
+git init
 
 # to remove everything -
 # rm -rf env/ .gitignore conda.yaml README.md .git/
